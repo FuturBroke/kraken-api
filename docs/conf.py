e3b25f805a01925ff7c1273954c8cd407ab82e91
Avel Docquin <adocquin@outlook.com>
2021-05-05 00:22:45 +0200

    Set package first alpha release tag.

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, Avel Docquin"
 author = "Avel Docquin"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.1"
+release = "1.0.0-alpha.1"
 
 
 # -- General configuration ---------------------------------------------------
