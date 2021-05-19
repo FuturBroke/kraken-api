2d32b465adcc5f67835555cad3cc542cf41478ec
Avel Docquin <adocquin@outlook.com>
2021-05-19 23:45:49 +0200

    KrakenAPI: In send_api_request handle connection reset error by waiting 0.5sc before retrying.

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, Avel Docquin"
 author = "Avel Docquin"
 
 # The full version, including alpha/beta/rc tags
-release = "1.0.0-alpha.4"
+release = "1.0.0-alpha.5"
 
 
 # -- General configuration ---------------------------------------------------
